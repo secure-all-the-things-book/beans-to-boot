99af3ee712b914771b75752e3f24943440d77014
Josh Long <joshua.long@broadcom.com>
2026-06-30 12:09:36 +0900

    beans to boot

---
+++ beans-to-boot/src/main/java/com/example/beans_to_boot/BeansToBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.beans_to_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeansToBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeansToBootApplication.class, args);
+	}
+
+}
---
+++ beans-to-boot/src/test/java/com/example/beans_to_boot/BeansToBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.beans_to_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansToBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
