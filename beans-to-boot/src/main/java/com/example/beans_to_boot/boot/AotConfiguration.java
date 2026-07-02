d9e26dbb3abe767584f4395aaff859409e418fb2
Josh Long <joshua.long@broadcom.com>
2026-07-03 03:24:39 +0900

    automatic save @ 2026-07-02T18:24:39.515777Z

--- beans-to-boot/src/main/java/com/example/beans_to_boot/boot/AotConfiguration.java
+++ beans-to-boot/src/main/java/com/example/beans_to_boot/boot/AotConfiguration.java
@@ -1,15 +1,39 @@
 package com.example.beans_to_boot.boot;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jdbc.core.dialect.JdbcPostgresDialect;
+import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import java.nio.charset.Charset;
 
 @Configuration
+// <.>
+@ImportRuntimeHints(AotConfiguration.Hints.class)
 class AotConfiguration {
 
+	// <.>
+	private static final Resource MESSAGE = new ClassPathResource("/message");
+
+	static class Hints implements RuntimeHintsRegistrar {
+
+		@Override
+		public void registerHints(RuntimeHints hints, @Nullable ClassLoader classLoader) {
+			// <.>
+			hints.resources().registerResource(MESSAGE);
+		}
+
+	}
+
+	// <.>
 	@Bean
-	JdbcPostgresDialect jdbcPostgresDialect() {
-		return JdbcPostgresDialect.INSTANCE;
+	ApplicationRunner messageLoader() {
+		return _ -> IO.println(MESSAGE.getContentAsString(Charset.defaultCharset()));
 	}
 
 }
